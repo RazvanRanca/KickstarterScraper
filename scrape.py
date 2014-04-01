af5167b4af79e0d5306aba996b389746d636a1bc
Razvan Ranca <rr463@cl.cam.ac.uk>
2014-04-01 03:59:14 +0100

    added parallel retrieval of all categs

--- scrape.py
+++ scrape.py
@@ -5,59 +5,88 @@ from datetime import timedelta
 from calendar import timegm
 import cPickle
 import math
+from multiprocessing import Pool
+import os
 
-def retrieveProjects(categ="technology", sort="magic", noProjs = 20):
-  projs = {}
+def retrieveProjects(categ="technology", sort="end_date", noProjs = 20):
   page = 0
+  extProjs = 0
+  seenPids = set()
   if not noProjs:
     noProjs = float("inf")
-  while noProjs > len(projs):
+  while noProjs > extProjs:
     page += 1
     r = requests.get("https://www.kickstarter.com/discover/categories/" + categ, params = {"sort":sort,"page":page})
+    while r.status_code != 200:
+      tm.sleep(1)
+      r = requests.get("https://www.kickstarter.com/discover/categories/" + categ, params = {"sort":sort,"page":page})
     soup = BeautifulSoup(r.text)
-
     if page == 1:
       totalProjs = int(soup.find("b", "count green").text.split()[0].encode('utf-8').translate(None,','))
       if noProjs > totalProjs:
         noProjs = totalProjs
         print "Total projects:", noProjs 
-
-    maxProjs = min(20, noProjs - len(projs))
-    print "Processing page", page, "- extracted", len(projs), "projects"
+    maxProjs = min(20, noProjs - extProjs)
     for proj in soup.find_all("div", "project-card-wrap")[:maxProjs]:
-      pid = int(proj["data-project"][6:-1]) # TODO: check if this id is actually unique per project
-      if pid in projs: # since the pages of results are queried one at a time a project can be read twice if it shifts pages
-        print "skipped"
-        continue
-      bn = proj.find("h2", "bbcard_name")
-      name = bn.find("a").text.strip()
-      author = bn.find("span").text[3:].strip()
-      blurb = proj.find("p", "bbcard_blurb").text.strip()
-      loc = proj.find("span", "location-name").text.strip()
-      percFund = float(proj.find("div", "project-pledged")["style"].split()[1][:-1])
-      money = proj.find("li", "pledged").contents[1]
-      currency =  money.contents[0]["class"][1]
-      amount = float(money.text[1:].encode('utf-8').translate(None,','))
-      time = proj.find("li", "last ksr_page_timer")["data-end_time"]
-      offHours, offMins = time[-5:].split(':')
-      offSecs = float(offHours)*3600 + float(offMins)*60
-      if time[-6] == "+":
-        offSecs *= -1
-      else:
-        assert(time[-6] == "-")
-      secsLeft = timegm(tm.strptime(time[:-6], "%Y-%m-%dT%H:%M:%S")) + offSecs - tm.mktime(tm.gmtime())
-      timeLeft = timedelta(seconds = secsLeft)
+      try:
+        pid = int(proj["data-project"][6:-1]) # TODO: check if this id is actually unique per project
+        if pid in seenPids: # since the pages of results are queried one at a time a project can be read twice if it shifts pages
+          print "skipped"
+          continue
+        bn = proj.find("h2", "bbcard_name")
+        name = bn.find("a").text.strip()
+        author = bn.find("span").text[3:].strip()
+        blurb = proj.find("p", "bbcard_blurb").text.strip()
+        loc = proj.find("span", "location-name").text.strip()
+        if proj.find("div", "project-status project-failed"):
+          projStatus = "Failed"
+          timeLeft = 0
+          percFund = 0           #This could have the amount pledged before the project was cancelled, but would have to load another page to retrieve it
+          pledged = ("USD", 0)
+        elif proj.find("div", "project-status project-canceled"):
+          projStatus = "Canceled"
+          timeLeft = 0
+          percFund = 0
+          pledged = ("USD", 0)
+        else:
+          percFund = float(proj.find("div", "project-pledged")["style"].split()[1][:-1])
+          if percFund >= 100:
+            projStatus = "Successful"
+          else:
+            projStatus = "Live"
 
-      projs[pid] = {"name":name, "author":author, "blurb":blurb, "location":loc, "pledged":(currency, amount), "percentFunded":percFund, "timeLeft":timeLeft}
+          money = proj.find("li", "pledged").contents[1]
+          currency =  money.contents[0]["class"][1]
+          amount = float(money.text[1:].encode('utf-8').translate(None,','))
+          time = proj.find("li", "last ksr_page_timer")["data-end_time"]
+          offHours, offMins = time[-5:].split(':')
+          offSecs = float(offHours)*3600 + float(offMins)*60
+
+          if time[-6] == "+":
+            offSecs *= -1
+          else:
+            assert(time[-6] == "-")
+          secsLeft = timegm(tm.strptime(time[:-6], "%Y-%m-%dT%H:%M:%S")) + offSecs - tm.mktime(tm.gmtime())
+          timeLeft = timedelta(seconds = secsLeft)
+          #print projStatus, timeLeft
+          if projStatus == "Live":
+            assert(timeLeft.total_seconds() >= 0)
+          
+
+        yield (pid, {"name":name, "author":author, "blurb":blurb, "location":loc, "pledged":(currency, amount), "percentFunded":percFund, "timeLeft":timeLeft, "status":projStatus})
+        seenPids.add(pid)
+        extProjs += 1
+      except:
+        print "Exception", categ, page, proj
+      #  assert(False)
+
+    if page % 10 == 0:
+      print categ, " - page", page, "- extracted", extProjs, "/", noProjs
 
 
-  return projs
-
-def storeProjects(projs, categ = None, fn = None):
-  if not fn:
-    fn = categ + str(len(projs))
+def storeProjects(projs, fn):
   with open("projectStores/" + fn, 'w') as f:
-    for i in projs.items():
+    for i in projs:
       cPickle.dump(i, f, cPickle.HIGHEST_PROTOCOL)
 
 def loadProjects(fn):
@@ -83,11 +112,39 @@ def totalPledged(projs, targetCurr = "GBP"):
 
   return sum([exchange[curr.upper()] * amm for (curr, amm) in [proj["pledged"] for proj in projs.values()]])
 
+def retrieveAndStore(categ, tp = "Par"):
+  storeProjects(retrieveProjects(categ, sort="end_date", noProjs=None), fn = categ + tp)
+
+def runSerial(categs):
+  rezs = []
+  for categ in categs:
+    retrieveAndStore(categ, "Ser")
+
+def runParallel(categs):
+  pool = Pool(10)
+  rez = pool.map_async(retrieveAndStore, categs)
+  rez.get()
+
+def compareVersions(categs):
+  timeStart = tm.time()
+  runSerial(categs)
+  print "Serial ran in:", tm.time() - timeStart
+
+  timeStart = tm.time()
+  runParallel(categs)
+  print "Parallel ran in:", tm.time() - timeStart
+
+  for categ in categs:
+    parProjs = loadProjects(categ + "Par")
+    serProjs = loadProjects(categ + "Ser")
+    for k in parProjs.keys():
+      del parProjs[k]["timeLeft"] 
+    for k in serProjs.keys():
+      del serProjs[k]["timeLeft"]
+    print categ, "Matches:", parProjs == serProjs
+
 
 if __name__ == "__main__":
-  categ = "art"
-  projs = retrieveProjects(categ, noProjs=None, sort="end_date")
-  print len(projs)
-  #storeProjects(projs, categ = categ)
-  #projs = loadProjects(categ + "20")
-  #print totalPledged(projs)
+  categs = ["art", "comics", "dance", "design", "fashion", "film%20&%20video", "food", "games", "music", "photography", "publishing", "technology", "theater"]
+  runParallel(categs)
+
